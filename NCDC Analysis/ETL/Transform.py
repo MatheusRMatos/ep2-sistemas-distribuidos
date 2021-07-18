405c7b6a4301892bce8145638cdf8460470b190f
MatheusRMatos <matheus-roschel@hotmail.com>
2021-07-18 16:23:55 -0300

    Métodos de Estatística descritiva

--- NCDC Analysis/ETL/Transform.py
+++ NCDC Analysis/ETL/Transform.py
@@ -1,13 +1,13 @@
 import pyspark.sql.types
-from pyspark.sql.functions import round, col
+from pyspark.sql.functions import round, col, when
 
 '''
 Arquivo de Transformação dos dados do DataFrame:
-    - Sistema universal de medidas
+    + Sistema universal de medidas
         + Transformar dados
-        - Tratar dos casos especiais
-    - Padronização dos reesultados para análise
-    - Transformação dos tipos de colunas
+        + Tratar dos casos especiais
+    + Transformação dos tipos de colunas
+    - Filtrar missing data
     
 '''
 
@@ -41,18 +41,18 @@ def transformColumns(df):
 
 # Transforma os dados de acordo com as unidades de medida universal
 def transformData(df):
-    df = df .withColumn("TEMP", (df.TEMP - 32) * 5 / 9).withColumn("TEMP", round(col("TEMP"), 2)) \
-            .withColumn("DEWP", (df.DEWP - 32) * 5 / 9).withColumn("DEWP", round(col("DEWP"), 2)) \
-            .withColumn("SLP", (df.SLP / 1023.25)).withColumn("SLP", round(col("SLP"), 2)) \
-            .withColumn("STP", (df.STP / 1023.25)).withColumn("STP", round(col("STP"), 2)) \
-            .withColumn("VISIB", (df.VISIB * 1.60934)).withColumn("VISIB", round(col("VISIB"), 2)) \
-            .withColumn("WDSP", (df.WDSP * 1.852)).withColumn("WDSP", round(col("WDSP"), 2)) \
-            .withColumn("MXSPD", (df.MXSPD * 1.852)).withColumn("MXSPD", round(col("MXSPD"), 2)) \
-            .withColumn("GUST", (df.GUST * 1.852)).withColumn("GUST", round(col("GUST"), 2)) \
-            .withColumn("MAX", (df.MAX - 32) * 5 / 9).withColumn("MAX", round(col("MAX"), 2)) \
-            .withColumn("MIN", (df.MIN - 32) * 5 / 9).withColumn("MIN", round(col("MIN"), 2)) \
-            .withColumn("PRCP", (df.PRCP * 25.4)).withColumn("PRCP", round(col("PRCP"), 2)) \
-            .withColumn("SNDP", (df.SNDP * 25.4)).withColumn("SNDP", round(col("SNDP"), 2))
+    df = df .withColumn("TEMP", when(df.TEMP >= 9999.9, df.TEMP).otherwise((df.TEMP - 32) * 5 / 9)).withColumn("TEMP", round(col("TEMP"), 2)) \
+            .withColumn("DEWP", when(df.DEWP >= 9999.9, df.DEWP).otherwise((df.DEWP - 32) * 5 / 9)).withColumn("DEWP", round(col("DEWP"), 2)) \
+            .withColumn("SLP", when(df.SLP >= 9999.9, df.SLP).otherwise(df.SLP / 1023.25)).withColumn("SLP", round(col("SLP"), 2)) \
+            .withColumn("STP", when(df.STP >= 9999.9, df.STP).otherwise((df.STP / 1023.25))).withColumn("STP", round(col("STP"), 2)).withColumn("SLP", round(col("SLP"), 2)) \
+            .withColumn("VISIB", when(df.VISIB >= 999.9, df.VISIB).otherwise((df.VISIB * 1.60934))).withColumn("VISIB", round(col("VISIB"), 2)) \
+            .withColumn("WDSP", when(df.WDSP >= 999.9, df.WDSP).otherwise((df.WDSP * 1.852))).withColumn("WDSP", round(col("WDSP"), 2)) \
+            .withColumn("MXSPD", when(df.MXSPD >= 999.9, df.MXSPD).otherwise((df.MXSPD * 1.852))).withColumn("MXSPD", round(col("MXSPD"), 2)) \
+            .withColumn("GUST", when(df.GUST >= 999.9, df.GUST).otherwise((df.GUST * 1.852))).withColumn("GUST", round(col("GUST"), 2)) \
+            .withColumn("MAX", when(df.MAX >= 9999.9, df.MAX).otherwise((df.MAX - 32) * 5 / 9)).withColumn("MAX", round(col("MAX"), 2)) \
+            .withColumn("MIN", when(df.MIN >= 9999.9, df.MIN).otherwise((df.MIN - 32) * 5 / 9)).withColumn("MIN", round(col("MIN"), 2)) \
+            .withColumn("PRCP", when(df.PRCP >= 99.99, df.PRCP).otherwise((df.PRCP * 25.4))).withColumn("PRCP", round(col("PRCP"), 2)) \
+            .withColumn("SNDP", when(df.SNDP >= 999.9, df.SNDP).otherwise((df.SNDP * 25.4))).withColumn("SNDP", round(col("SNDP"), 2))
     return df
 
 # Transforma colunas String nos tipos adequados
--- NCDC Analysis/main.py
+++ NCDC Analysis/main.py
@@ -1,6 +1,8 @@
 from pyspark.sql import SparkSession
+from pyspark.sql import functions as F
 from ETL.Extract import *
 from ETL.Transform import *
+from Statistic.Descritive import *
 
 spark = SparkSession \
     .builder \
@@ -17,10 +19,24 @@ df = transformColumns(df)
 df.show(5)
 # print("O dataframe agora tem ",df.count()," linhas e ", len(df.columns), "colunas. \n")
 df = transformData(df)
+# df = df.withColumn("TEMP", when(df.TEMP == 17.89, 231546).otherwise(df.TEMP))
 df.show(5)
 '''
 print("Digite a coluna que deseja agrupar: ")
 campo = input()
 '''
-# print(df.filter(df["TEMP_ATTRIBUTES"] == "24").show())
+# print(df.filter(df["SLP"] >= 9999.9).show())
 # print(df.groupBy(campo).count().orderBy("count", ascending=True).show())
+'''
+df_stats = df.select(
+    F.mean(F.col('TEMP')).alias('mean'),
+    F.stddev(F.col('TEMP')).alias('std')
+).collect()
+
+mean = df_stats[0]['mean']
+std = df_stats[0]['std']
+'''
+
+print("A média é: ", media(df,'TEMP'))
+print("O desvio é: ", desvioPadrao(df, 'TEMP'))
+print("A mediana é: ", mediana(df, 'TEMP'))
